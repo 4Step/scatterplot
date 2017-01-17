9393821dc3cd76eab1006f59510f4cf04c30c7c7
Sarvepalli <Sarvepalli@pbworld.com>
2017-01-17 10:40:18 -0500

    with outputs

--- scatterplot.r
+++ scatterplot.r
@@ -7,21 +7,22 @@
   library(dplyr)
   
   df <- read.dbf(dataFile)
+  
   df <- df %>% 
-        mutate(diff = DAILYBOARD - DAY_ON) %>%
+        filter(is.na(FGSTP)) %>%
+        mutate(diff = DAILYBOARD - DAY_TOT) %>%
         mutate(bin = cut(diff, 
                         breaks = c(min(diff), -500, -100, -50, -20, 0, 20, 50, 100, 500, max(diff))),
                         include.lowest = TRUE) %>%
         mutate(bin = paste(
                        as.numeric(sub("\\((.+),.*", "\\1", bin )), 
                        as.numeric(sub("[^,]*,([^]]*)\\]", "\\1", bin )),
-                       sep=" to ")) %>%
-        select(ROUTE, STOPNAME, STATION, DAILYBOARD, DAY_ON, diff, bin)
+                       sep=" to "))
   
   View(df)
   
   # scatterplot
-  scatterplot <- ggplot(df, aes(DAILYBOARD, DAY_ON)) +
+  scatterplot <- ggplot(df, aes(DAILYBOARD, DAY_TOT)) +
     geom_point(aes(colour = factor(ROUTE))) +
     geom_smooth(method ="lm") +
     coord_cartesian() +
@@ -29,11 +30,25 @@
     theme_bw()
   
   # Compute R-Squared
-  m <- lm(DAILYBOARD ~ DAY_ON, df)
+  m <- lm(DAILYBOARD ~ DAY_TOT, df)
   rsq <- paste0("R-Squared = ", round(summary(m)$r.squared,4))
   
   # Add R-Squared to plot    
   scatterplot <- scatterplot + 
                  geom_text(x = 2000, y = 500, label =rsq, parse = TRUE)
-    
+  
+  
+  df_route <- df %>% group_by(ROUTE) %>%
+    summarise(apc = sum(DAY_TOT), stops = sum(DAILYBOARD))
+ 
+  df_agency <- df %>% group_by(Agency) %>%
+    summarise(apc = sum(DAY_TOT), stops = sum(DAILYBOARD))
    
+  # write out the difference
+  write.csv(df, "df.csv")
+  write.csv(df_route, "df_route.csv")
+  
+
+  
+  
+  
